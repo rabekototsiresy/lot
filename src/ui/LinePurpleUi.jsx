8698d80cb69d80c080c96e82f44dbc244af904cf
Tsiresy RABEKOTO <tsiresyrabekoto@gmail.com>
2020-12-14 17:26:43 +0100

    fix : fix bottom elements

--- src/ui/LinePurpleUi.jsx
+++ src/ui/LinePurpleUi.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const LinePurpleUi = () => {
     return (
         <div className="line-purple">
-            <span>X</span>
-            <span>Annuler la creation du compte</span>
-            <span>x</span>
-            <span>Retour</span>
+           
         </div>
     )
 }
--- src/ui/PopupBottomUi.jsx
+++ src/ui/PopupBottomUi.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const PopupBottomUi = () => {
     return (
         <div className="popup-bottom">
+             <div className="content-bottom-text">
+                <span>X</span>
+                <span>Annuler la creation du compte</span>
+             </div>
             
+            <div className="content-bottom-right">
+                <span>x</span>
+                <span>Retour</span>
+            </div>
         </div>
     )
 }
